9eb037ced8c25077202ebb70f15578899ec54ab9
Cooper Sawyer <cooper.sawyer@powerhrg.com>
2021-07-01 11:29:13 -0400

    stretch goal delivered

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,10 +1,26 @@
 import React from "react";
 
-const Article = ({ title, date = `January 1, 1970`, preview}) => {
+const emojiList = (minutes) => {
+    const time = minutes < 30 ? 5 : 10
+    const emoji =  minutes < 30 ? "☕️" : "🍱"
+
+    let emojis = " "
+    for (let i = 0; i < minutes; i += time) {
+        emojis += emoji
+    }
+    return emojis
+}
+
+const Article = ({ 
+    title, 
+    date = `January 1, 1970`, 
+    preview, 
+    minutes}) => {
+    
     return (
         <article>
             <h3>{title}</h3>
-            <small>{date}</small>
+            <small>`{date} {emojiList(minutes)} {minutes} minutes read`</small>
             <p>{preview}</p>
         </article>
 
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Article from "./Article"
 
-function ArticleList({posts}) {
+const ArticleList = ({posts}) => {
     const articles = posts.map((posts) => (
         <Article 
             key={posts.id}
             title={posts.title}
             date={posts.date} 
             preview={posts.preview}
+            minutes={posts.minutes}
             />
     ))     
         return <main>
